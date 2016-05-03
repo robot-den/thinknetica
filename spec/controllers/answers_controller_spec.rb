131eeed58af17f8611db1ca61211886d74558883
robot-den <w.nazmutdinov@gmail.com>
2016-05-03 18:23:25 +0500

    fixed destroy tests for answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -95,15 +95,33 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'DELETE #destroy' do
     sign_in_user
-    let(:destroy_answer) { delete :destroy, question_id: answer.question_id, id: answer }
-    it "delete answer from database" do
-      answer
-      expect { destroy_answer }.to change(Answer, :count).by(-1)
+
+    context 'by the author of the answer' do
+      let(:answer) { Answer.create!(body: 'ExampleBody',question_id: question.id, user_id: @user.id) }
+
+      it "delete answer from database" do
+        answer
+        expect { delete :destroy, id: answer }.to change(Answer, :count).by(-1)
+      end
+
+      it "redirect to question show view with notice" do
+        delete :destroy, id: answer
+        expect(response).to redirect_to question_url(answer.question_id)
+        expect(flash[:notice]).to be_present
+      end
     end
 
-    it "redirect to question show view" do
-      destroy_answer
-      expect(response).to redirect_to question_url(answer.question_id)
+    context "by not the author of the answer" do
+      it "doesnt deletes answer from database" do
+        answer
+        expect { delete :destroy, id: answer }.to_not change(Question, :count)
+      end
+
+      it "redirects to question show view with notice" do
+        delete :destroy, id: answer
+        expect(response).to redirect_to question_path(answer.question_id)
+        expect(flash[:notice]).to be_present
+      end
     end
   end
 end
