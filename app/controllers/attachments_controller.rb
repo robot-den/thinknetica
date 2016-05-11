97f74de110fecca590ad92806e556583dbd192e5
robot-den <w.nazmutdinov@gmail.com>
2016-05-12 01:18:21 +0500

    refactoring attachment controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,8 +3,7 @@ class AttachmentsController < ApplicationController
 
   def destroy
     @attachment = Attachment.find(params[:id])
-    attachable = @attachment.attachable_type.constantize.find(@attachment.attachable_id)
-    @attachment.destroy if current_user.id == attachable.user_id
+    @attachment.destroy if current_user.id == @attachment.attachable.user_id
   end
 
 end
