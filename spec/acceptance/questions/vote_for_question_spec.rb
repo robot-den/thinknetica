1b9e166b6579bd1e4dfb91c43cd099e769387ea9
robot-den <w.nazmutdinov@gmail.com>
2016-05-15 21:02:58 +0500

    added red acceptance tests for feature: user can vote up or down for question

---
+++ spec/acceptance/questions/vote_for_question_spec.rb
@@ -0,0 +1,61 @@
+require "acceptance_helper"
+
+feature 'user can vote for question', %q{
+  In order to estimate question
+  As an authenticated user
+  I want to vote up or down for it
+} do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  scenario 'authenticated user vote up, cancel vote and vote down' do
+    sign_in(user)
+    visit question_path(question)
+
+    within ".question .rating" do
+      click_on 'up'
+
+      expect(page).to have_content '1'
+      expect(page).to have_link 'cancel'
+      expect(page).to_not have_link 'up'
+      expect(page).to_not have_link 'down'
+
+      click_on 'cancel'
+
+      expect(page).to have_content '0'
+      expect(page).to_not have_link 'cancel'
+      expect(page).to have_link 'up'
+      expect(page).to have_link 'down'
+
+      click_on 'down'
+
+      expect(page).to have_content '-1'
+      expect(page).to have_link 'cancel'
+      expect(page).to_not have_link 'up'
+      expect(page).to_not have_link 'down'
+    end
+  end
+  scenario "authenticated user vote for his own question"do
+    sign_in(user)
+    question = create(:question, user: user)
+    visit question_path(question)
+
+    within ".question .rating" do
+      expect(page).to have_content '0'
+      expect(page).to_not have_link 'cancel'
+      expect(page).to_not have_link 'up'
+      expect(page).to_not have_link 'down'
+    end
+  end
+
+  scenario 'non-authenticated user try vote' do
+    visit question_path(question)
+
+    within ".question .rating" do
+      expect(page).to have_content '0'
+      expect(page).to_not have_link 'cancel'
+      expect(page).to_not have_link 'up'
+      expect(page).to_not have_link 'down'
+    end
+  end
+end
