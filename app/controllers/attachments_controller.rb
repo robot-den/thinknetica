b94e891997e2bfd5c12f4a1aff42f4f1c8c5af30
robot-den <w.nazmutdinov@gmail.com>
2016-05-28 20:25:52 +0500

    attachments controller refactoring with respond_with

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,9 +1,18 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_attachment, only: :destroy
+
+  respond_to :js
 
   def destroy
-    @attachment = Attachment.find(params[:id])
     @attachment.destroy if current_user.id == @attachment.attachable.user_id
+    respond_with @attachment
+  end
+
+  private
+
+  def get_attachment
+    @attachment = Attachment.find(params[:id])
   end
 
 end
