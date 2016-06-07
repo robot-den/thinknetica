c7b01900bce376401f6690fc2e1eecb0cc0b079c
robot-den <w.nazmutdinov@gmail.com>
2016-06-07 22:14:50 +0500

    refactored views with cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,12 @@ class Ability
 
   def initialize(user)
     if user
+      alias_action :vote_up, :vote_down, :vote_cancel, to: :vote
+
       can :read, Question
       can :create, [Question, Answer, Comment]
       can [:update, :destroy], [Question, Answer], user: user
-      can [:vote_up, :vote_down, :vote_cancel], [Question, Answer] { |votable| votable.user != user }
+      can [:vote], [Question, Answer] { |votable| votable.user != user }
       can :set_as_best, Answer, question: { user: user }
       can :destroy, Attachment, attachable: { user: user }
     else
