e2a7b061af3546fcbc0e15974c5e2845062d3f27
robot-den <w.nazmutdinov@gmail.com>
2016-06-26 21:35:41 +0500

    added search action with tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,18 @@
 class SearchController < ApplicationController
+  def search
+    if resource == 'everywhere'
+      @search_results = ThinkingSphinx.search params[:query]
+    elsif resource
+      @search_results = resource.classify.constantize.search params[:query]
+    else
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def resource
+    whitelist = %{ everywhere, questions, answers, comments, users }
+    whitelist.include?(params[:search_by]) ? params[:search_by] : nil
+  end
 end
