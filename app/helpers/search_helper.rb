677e5193dc933606f63941dfe1f6075349e49255
robot-den <w.nazmutdinov@gmail.com>
2016-06-26 17:46:21 +0500

    rewrite search form with rails form helpers, created route

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,9 @@
 module SearchHelper
+  def search_types
+    [['everywhere', 'everywhere'],
+     ['questions', 'questions'],
+     ['answers', 'answers'],
+     ['comments', 'comments'],
+     ['users', 'users']]    
+  end
 end
