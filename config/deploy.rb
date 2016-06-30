6695c1e22c56aae2a8bce505146e547365daa228
robot-den <w.nazmutdinov@gmail.com>
2016-06-30 23:18:13 +0500

    add capistrano files

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'thinknetica'
+set :repo_url, 'git@github.com:robot-den/thinknetica.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/qna'
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/private_pub.yml', '.env')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
