526f61d522bf3cc1d43068bc88341ffa9d1e5017
robot-den <w.nazmutdinov@gmail.com>
2016-07-10 23:21:10 +0500

    cache for index and show question

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Answer, type: :model do
     it { should validate_presence_of :question_id }
     it { should validate_presence_of :user_id }
     it { should validate_length_of(:body).is_at_least(10) }
-    it { should belong_to(:question) }
+    it { should belong_to(:question).touch(true) }
     it { should belong_to(:user) }
     it { should have_many(:attachments).dependent(:destroy) }
     it { should have_many(:votes).dependent(:destroy) }
